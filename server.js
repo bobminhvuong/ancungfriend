9466a4a03797021ffeee8f35e5d9c0b2ce98f1c2
Nguyen minh vuong <vietduchelo@gmail.com>
2018-07-20 14:23:53 +0700

    new

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ app.use(config.BASE_URL+'/auth', require('./routes/auth.route')());
 app.use(config.BASE_URL+'/party', require('./routes/party.route')());
 app.use(config.BASE_URL+'/restaurant', require('./routes/restaurant.route')());
 app.use(config.BASE_URL+'/typefood', require('./routes/typefood.route')());
+app.use('/',function(req,res){
+    res.send('API An Cung');
+});
+
 
 app.listen(process.env.PORT || 3000,console.log('serve is listening port '));
 
